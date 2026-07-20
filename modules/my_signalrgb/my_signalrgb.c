08ebfba59a6f1597291ae837a0fcc56da32cb651
Nathan Bellows <nabellows@gmail.com>
2026-07-20 11:01:41 -0700

    Improvement for SignalRGB effect inconsistency
    Ensures SignalRGB effect is properly set even in intermediate states
    (unsure why sometimes its off, though I definitely was abusing the
    enable toggle in the plugin, possibly breaking Signal)

--- modules/my_signalrgb/my_signalrgb.c
+++ modules/my_signalrgb/my_signalrgb.c
@@ -1,6 +1,20 @@
 #include <stdbool.h>
 #include <stdint.h>
-bool via_command_user(uint8_t src, uint8_t *data, uint8_t length) {
-    extern bool via_command_signalrgb(uint8_t *data, uint8_t length);
-    return via_command_signalrgb(data, length);
+#include "rgb_matrix.h"
+#include "signalrgb.h"
+
+extern bool via_command_signalrgb(uint8_t *data, uint8_t length);
+
+__attribute__((weak)) bool via_command_user(uint8_t src, uint8_t *data, uint8_t length) {
+    if (rgb_matrix_is_enabled()) {
+        if (data[0] == STREAM_RGB_DATA && rgb_matrix_get_mode() != RGB_MATRIX_SIGNALRGB) {
+            rgb_matrix_mode_noeeprom(RGB_MATRIX_SIGNALRGB);
+        }
+        if (via_command_signalrgb(data, length)) {
+            extern bool via_raw_hid_send(uint8_t src, uint8_t *data, uint8_t length);
+            via_raw_hid_send(src, data, length);
+            return true;
+        }
+    }
+    return false;
 }
